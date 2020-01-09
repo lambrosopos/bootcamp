35448058073d26260c19583ec01c6344daec24ad
lambrosopos <qetroux@Tomarks-MacBook-Pro.local>
2020-01-09 21:40:14 +0900

    Added a recursive function problem

--- jsToy.js
+++ jsToy.js
@@ -89,3 +89,70 @@ console.log(flatten2([1,2,[3],[4]]));
 
 
 
+
+// ============================================
+// Week 5-1 multiplicativePersistence
+// ============================================
+
+
+// 양의 정수가 주어졌을때, multiplicativePersistence(num) 함수는 주어진 수의 각 자리의 숫자를 곱했을때 한 자릿수가 될때까지의 계산 횟수를 반환합니다.
+
+// 예시: 만약 입력값이 39라면, 여러분의 함수는 3을 반환해야 합니다.
+
+// 왜냐하면
+
+// 3 * 9 = 27
+
+// 그리고 2 * 7 = 14
+
+// 그리고 마지막으로 1 * 4 = 4 이므로 한 자리가 될 때까지 총 3번의 계산을 하기 때문입니다.
+
+function multiplyNumbers(num) {
+    return String(num).split('').reduce((acc, curr) => acc * curr, 1)
+}
+
+function multiplicativePersistence(num) {
+    // your code here
+    // check if number is bigger than 9
+    //      if bigger than 9:
+    //          multiply all the single numbers within num
+    //              - split all the numbers
+    //              - multiply them all
+    //              - call on this function again
+    //      else:
+    //          give back number and end function
+    //      keep doing this until number is less than 10
+    newNum = String(num).split('').reduce((acc, curr) => acc * curr, 1)
+    
+    newNum > 9 ? 
+
+
+
+
+
+    // let stringNum = String(num);
+    // console.log("Starting number " + stringNum)
+    // if (stringNum.length > 1){
+    //     let result = 1
+    //     for (i of stringNum){
+    //         result = result * parseInt(i);
+    //         console.log("Single Char " + parseInt(i))
+    //         console.log("Result " + result)
+    //         if (result === 0){
+    //             console.log("Returning 1")
+    //             return 1
+    //         }
+    //     }
+    //     num = multiplicativePersistence(result)
+    // } else {
+    //     console.log("Number " + num)
+    //     return num
+    // }
+}
+
+console.log(multiplyNumbers(2312))
+console.log(multiplyNumbers(786))
+console.log(multiplyNumbers(2248))
+console.log(multiplyNumbers(516))
+console.log(multiplyNumbers(2300))
+
