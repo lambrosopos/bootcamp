2350e65e3830b9a99f3b46ac763c31f5df17ed82
lambrosopos <qetroux@Tomarks-MacBook-Pro.local>
2020-01-10 08:55:05 +0900

    New Problem : getProductOfAllElementsAtProperty
    Dealing with objects and arrays

--- jsToy.js
+++ jsToy.js
@@ -350,3 +350,29 @@ console.log(sumOnMultiplicationOfPairs(2, 5, 3, 2, 3));
 console.log(sumOnMultiplicationOfPairs(3, 3, 4, 2, 7, 1));
 
 // Solution is different -> two ways : one with unique set, the other not
+
+
+
+// ============================================
+// "getProductOfAllElementsAtProperty" 함수를 작성하세요.
+// ============================================
+
+// 객체와 키가 주어졌을때, "getProductOfAllElementsAtProperty" 함수는 주어진 키에 위치한 배열의 요소의 곱을 반환합니다.
+
+// Notes:
+
+// 만약 배열이 비어있다면, 0을 반환해야 합니다.
+// 만약 주어진 키에 위치하는 속성값이 배열이 아니라면, 0을 반환해야 합니다.
+// 만약 주어진 키에 위치하는 속성이 없다면, 0을 반환해야 합니다.
+// let obj = {
+//   key: [1, 2, 3, 4]
+// };
+// let output = getProductOfAllElementsAtProperty(obj, 'key');
+// console.log(output); // --> 24
+// Hint:
+
+// 배열의 reduce 메소드를 이용해보세요.
+
+function getProductOfAllElementsAtProperty(obj, key) {
+    // your code here
+  }
