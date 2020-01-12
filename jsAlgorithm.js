ba3780925aaf8fcf1ab70f750213d48b4f77e0a3
lambrosopos <qetroux@Tomarks-MacBook-Pro.local>
2020-01-13 02:54:49 +0900

    Problem Solution : Sum of Digits
    Methods : String.split(), Array.slice(), Array.reduce(), parseInt()
    
    Notes : Simplifying is not always a good thing

--- jsAlgorithm.js
+++ jsAlgorithm.js
@@ -337,8 +337,71 @@ console.log(getIndexOf('a', 'I\'m an apple'))
 
 function getIndexOf2(char, str) {
     // your code here
-    
+    return str.split('').reduce((acc, curr, curIdx) => curr===char ? (acc.push(curIdx), acc) : acc, [])[0]
 }
     
 
-console.log(getIndexOf2('a', 'I\'m an apple'))
+console.log(typeof getIndexOf2('a', 'I\'m an apple'))
+console.log(getIndexOf2('a', 'Crisis comes at an early price'))
+
+
+
+
+//========================================
+// a005_sumDigits.js
+//========================================
+/*
+초기화
+테스트 실행
+
+Write a function called "sumDigits". ("sumDigits" 함수를 작성하세요.)
+
+Given a number, "sumDigits" returns the sum of all its digits. (숫자가 주어졌을때, "sumDigits" 함수는 숫자의 각 자리수를 모두 더한 값을 반환합니다.)
+
+let output = sumDigits(1148);
+console.log(output); // --> 14
+If the number is negative, the first digit should count as negative. (만약 숫자가 음수라면, 첫번째 자릿수는 음수로 고려되어야 합니다.)
+
+let output = sumDigits(-316);
+console.log(output); // --> 4
+Notes:
+
+In order to use some of the methods that will be most helpful to you, you will most likely want to do some string to number conversion and vice versa. (- 여러분에게 도움이 될 메소드를 사용하기위해서는 아마도 문자열을 숫자로 바꾸거나 숫자를 문자로 바꾸는게 수월하실 겁니다.)
+Be sure to familiarize yourself with the "toString" method, as well as the "Number" function. ("Number" 함수와 "toString" 메소드에 꼭 익숙해지세요.)
+*/
+
+function summation(stringy){
+    return stringy.split('').reduce(function(acc, curr){
+      return acc + Number(curr)
+    }, 0);
+  }
+  
+  function sumDigits(num) {
+    // your code here
+    if (String(num)[0] === '-'){
+      return Number(summation(String(num).slice(2))) + Number(String(num).substring(0, 2))
+    } else {
+      return Number(summation(String(num)))
+    }
+  
+  }
+
+console.log(sumDigits(1423))
+console.log(sumDigits(-423))
+
+  function sumDigits2(num) {
+    // your code here
+    if (num < 0){
+      return Number(String(num).split('').slice(1).reduce((acc, curr, currIdx) => {
+        return currIdx === 0 ? acc-curr : acc + parseInt(curr)
+    }, 0))
+    } else {
+      return Number(String(num).split('').reduce((acc, curr) => {
+          return acc + parseInt(curr)
+      }, 0))
+    }
+  
+  }
+
+console.log(sumDigits2(7777))
+console.log(sumDigits2(-55024))
