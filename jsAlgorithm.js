75c9ef53e782c7380aaefe640e982ca64b00ca76
lambrosopos <qetroux@Tomarks-MacBook-Pro.local>
2020-01-12 18:04:44 +0900

    New Problem : Convert Object to List

--- jsAlgorithm.js
+++ jsAlgorithm.js
@@ -138,3 +138,49 @@ let dictB = {...dictA, [arrA[0]]:arrA[1]}
 console.log(dictA)
 console.log(dictB)
 
+
+
+
+//========================================
+// 006_convertObjectToArray3.js
+//========================================
+/*
+초기화
+테스트 실행
+
+Write a function called "convertObjectToList" which converts an object literal into an array of arrays, like this: (객체를 입력으로 받고 해당 객체를 다음과 같이 2차원 배열로 변형 할 수 있는 함수 "convertObjectToList"를 작성하세요.)
+
+Argument:
+
+{
+name: 'Holly',
+age: 35,
+role: 'producer'
+}
+Return value:
+
+[['name', 'Holly'], ['age', 35], ['role', 'producer']];
+Note that your function should be able to handle ANY object like this, not just the exact sample provided above. (여러분이 작성하신 함수는 위에 제시된 예시 뿐만 아니라 어떠한 객체도 다룰 수 있어야 합니다.)
+*/
+let objA = {
+    name: 'Holly',
+    age: 35,
+    role: 'producer'
+    }
+
+
+function convertObjectToList(obj) {
+    // your code here
+    let result= [];
+    for (key in obj){
+      result.push([key, obj[key]])
+    }
+    return result;
+  }
+  
+console.log(convertObjectToList(objA));
+
+function convertObjectToList2(obj) {
+    
+  }
+  
