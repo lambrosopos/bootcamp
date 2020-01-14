25770a94e91a62e5c7b788aac4144047e4ab6f40
lambrosopos <qetroux@Tomarks-MacBook-Pro.local>
2020-01-14 23:06:51 +0900

    New File : Koans Extra Credit problems

---
+++ javaKoansExtraCredit.js
@@ -0,0 +1,12 @@
+/*
+"should find the largest prime factor of a composite number"
+
+"should find the largest palindrome made from the product of two 3 digit numbers"
+
+"should find the smallest number divisible by each of the numbers 1 to 20"
+
+"should find the difference between the sum of the squares and the square of the sums"
+
+"should find the 10001st prime"
+*/
+
