1a46eabb245fdbb99bd0e4bd67f18c35086bebb1
lambrosopos <qetroux@Tomarks-MacBook-Pro.local>
2020-01-15 17:56:43 +0900

    Updated Problems and Regex

--- jsProblems/basicsOfJave.js
+++ jsProblems/basicsOfJave.js
@@ -89,3 +89,52 @@ result = arrA.reduce(function(acc, curr){
 }, 0);
 
 result
+
+
+//===============================================================
+// Seven Boom!
+//===============================================================
+
+/*
+Create a function that takes an array of numbers and return "Boom!" if the number 7 appears in the array. Otherwise, return "there is no 7 in the array".
+
+Examples
+sevenBoom([1, 2, 3, 4, 5, 6, 7]) ➞ "Boom!"
+
+sevenBoom([8, 6, 33, 100]) ➞ "there is no 7 in the array"
+
+sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
+Notes
+N/A
+*/
+
+function sevenBoom(arr) {
+   let newArr = arr.map((val) => String(val))
+
+    return newArr.map((val) => {
+        return val.split('').includes('7')})
+        .some((val) => val) ? 'Boom!' : "there is no 7 in the array";
+}
+
+console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]))
+console.log(sevenBoom([8, 6, 33, 100]))
+console.log(sevenBoom([2, 55, 60, 97, 86]))
+
+function sevenBoom2(arr) {
+  return /7/.test(arr) ? 'Boom!' : 'there is no 7 in the array';
+}
+
+console.log(sevenBoom2([1, 2, 3, 4, 5, 6, 7]))
+console.log(sevenBoom2([8, 6, 33, 100]))
+console.log(sevenBoom2([2, 55, 60, 97, 86]))
+
+function testingRegex(something) {
+    let re = /fell/g
+    let result = re.test(something)
+    let lastIndex = re.lastIndex
+    
+    return [result, lastIndex];
+}
+
+console.log(testingRegex('An apple fell off a tree'))
+console.log(testingRegex(['An', 'apple', 'fell', 'off', 'a', 'tree']))
