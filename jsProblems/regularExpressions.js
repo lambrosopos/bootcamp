4ff8d07ba6a45b6a348a36e9b06a64e6304c7edd
lambrosopos <qetroux@Tomarks-MacBook-Pro.local>
2020-01-15 17:59:01 +0900

    Regex

---
+++ jsProblems/regularExpressions.js
@@ -0,0 +1,13 @@
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
+
+
+var myRe = /ab*/g;
+var str = 'abbcdefabh';
+var myArray;
+while ((myArray = myRe.exec(str)) !== null) {
+  var msg = 'Found ' + myArray[0] + '. ';
+  msg += 'Next match starts at ' + myRe.lastIndex;
+  console.log(msg);
+}
+
+
