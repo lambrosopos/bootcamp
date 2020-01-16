2fbc3683f0a0d7747edf356048d1f3f34ca022d4
lambrosopos <lambrosopos@gmail.com>
2020-01-16 11:10:09 +0900

    JS class update

--- jsProblems/basicsOfJave.js
+++ jsProblems/basicsOfJave.js
@@ -6,7 +6,7 @@
 let dictA = {};
     dictA.key = '123'; // Using dot notation is closer to 'string'
 
-dictA; // {key: '123'}
+// dictA; // {key: '123'}
 
 // If using a bracket notation, the element inside the bracket
 // is considered as a variable
@@ -16,7 +16,7 @@ let key = 'key';
 let dictB = {};
     dictB[key] = '456';
 
-dictB; // {key: '456'}
+// dictB; // {key: '456'}
 
 
 // ============================================
@@ -35,14 +35,14 @@ if (5%2===1){
     oddOrEven = 'even';
 };
 
-oddOrEven;
+// oddOrEven;
 
 // This simple example can be changed into...
 // ...a single line
 
-oddOrEven = 5%2===1 ? 'odd' : 'even';
+// oddOrEven = 5%2===1 ? 'odd' : 'even';
 
-oddOrEven;
+// oddOrEven;
 
 
 
@@ -75,7 +75,7 @@ for (element of arrA) {
     result = result + element;
 };
 
-result;
+// result;
 
 // Instead of declaring result, then going to for loop, and then giving back result,
 // you can simply use reduce method
@@ -88,7 +88,7 @@ result = arrA.reduce(function(acc, curr){
     return acc + curr;
 }, 0);
 
-result
+// result
 
 
 //===============================================================
@@ -116,17 +116,17 @@ function sevenBoom(arr) {
         .some((val) => val) ? 'Boom!' : "there is no 7 in the array";
 }
 
-console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]))
-console.log(sevenBoom([8, 6, 33, 100]))
-console.log(sevenBoom([2, 55, 60, 97, 86]))
+// console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]))
+// console.log(sevenBoom([8, 6, 33, 100]))
+// console.log(sevenBoom([2, 55, 60, 97, 86]))
 
 function sevenBoom2(arr) {
   return /7/.test(arr) ? 'Boom!' : 'there is no 7 in the array';
 }
 
-console.log(sevenBoom2([1, 2, 3, 4, 5, 6, 7]))
-console.log(sevenBoom2([8, 6, 33, 100]))
-console.log(sevenBoom2([2, 55, 60, 97, 86]))
+// console.log(sevenBoom2([1, 2, 3, 4, 5, 6, 7]))
+// console.log(sevenBoom2([8, 6, 33, 100]))
+// console.log(sevenBoom2([2, 55, 60, 97, 86]))
 
 function testingRegex(something) {
     let re = /fell/g
@@ -136,5 +136,99 @@ function testingRegex(something) {
     return [result, lastIndex];
 }
 
-console.log(testingRegex('An apple fell off a tree'))
-console.log(testingRegex(['An', 'apple', 'fell', 'off', 'a', 'tree']))
+// console.log(testingRegex('An apple fell off a tree'))
+// console.log(testingRegex(['An', 'apple', 'fell', 'off', 'a', 'tree']))
+
+
+// Class in JS
+
+
+class Person {
+    constructor(first, last, age, gender, interests) {
+        this.name = {
+        first,
+        last
+        };
+        this.age = age;
+        this.gender = gender;
+        this.interests = interests;
+    }
+
+    greeting() {
+        console.log(`Hi! I'm ${this.name.first}`);
+    };
+
+    farewell() {
+        console.log(`${this.name.first} has left the building. Bye for now!`);
+    };
+}
+
+let han = new Person('Han', 'Solo', 25, 'male', ['Smuggling']);
+han.greeting();
+// Hi! I'm Han
+
+let leia = new Person('Leia', 'Organa', 19, 'female' ['Government']);
+leia.greeting();
+// Leia has left the building. Bye for now
+
+// Two inheritance methods
+
+// 1
+// class Teacher extends Person {
+//     constructor(first, last, age, gender, interests, subject, grade) {
+//       this.name = {
+//         first,
+//         last
+//       };
+  
+//     this.age = age;
+//     this.gender = gender;
+//     this.interests = interests;
+//     // subject and grade are specific to Teacher
+//     this.subject = subject;
+//     this.grade = grade;
+//     }
+//   }
+
+  // 2) Using Super()
+  class Teacher extends Person {
+    constructor(first, last, age, gender, interests, subject, grade) {
+      super(first, last, age, gender, interests);
+  
+      // subject and grade are specific to Teacher
+      this.subject = subject;
+      this.grade = grade;
+    }
+  }
+
+// Getters and setters
+
+
+
+class Snapey extends Person {
+    constructor(first, last, age, gender, interests, subject, grade) {
+      super(first, last, age, gender, interests);
+      // subject and grade are specific to Teacher
+      this._subject = subject;
+      this.grade = grade;
+    }
+  
+    get subject() {
+      return this._subject;
+    }
+  
+    set subject(newSubject) {
+      this._subject = newSubject;
+    }
+}
+
+let snape = new Snapey('Severus', 'Snape', 58, 'male', ['Potions'], 'Dark arts', 5);
+snape.greeting(); // Hi! I'm Severus.
+snape.farewell(); // Severus has left the building. Bye for now.
+snape.age // 58
+snape.subject; // Dark arts
+
+console.log(snape._subject)
+snape._subject = "Balloon Magic"
+console.log(snape._subject)
+
