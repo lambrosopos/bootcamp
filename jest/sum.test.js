6035c9b6cd2f0bd12857ef36c6c1e3d5b96dbe55
lambrosopos <lambrosopos@gmail.com>
2020-02-23 18:20:52 +0900

    added more tests

--- jest/sum.test.js
+++ jest/sum.test.js
@@ -3,3 +3,11 @@ const SUM = require('./sum.js');
 test('adds 1 + 2 to equal 3', () => {
   expect(SUM(1, 2)).toBe(3);
 });
+
+test('adds -1 + 2 to equal 1', () => {
+  expect(SUM(-1, 2)).toBe(1);
+});
+
+test('undefined + number should be NaN', () => {
+  expect(SUM(undefined, 2)).toBe(NaN);
+});
